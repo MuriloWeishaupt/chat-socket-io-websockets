f99b5de52a62d3e0a047dd55b4ce4f04912d0463
MWeishaupt <113941985+MuriloWeishaupt@users.noreply.github.com>
2025-06-05 01:38:05 +0000

    Adicionada abertura e fechamento de conexões em tempo real

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
-const express = require("express")
-const path = require("path")
+import express from "express";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { Server } from "socket.io"
+import { createServer } from "http";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const server = createServer(app);
+const io = new Server(server);
+const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
 
-app.use(express.static(path.join(__dirname, "public")))
+
+app.use(express.static(join(__dirname, "public"))); 
+
+let socketsConnected = new Set()
+
+io.on("connection", onConnected)
+
+function onConnected(socket) {
+  console.log(socket.id)
+  socketsConnected.add(socket.id)
+
+  io.emit("clients-total", socketsConnected.size)
+
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected", socket.id)
+    socketsConnected.delete(socket.id)
+    io.emit("clients-total", socketsConnected.size)
+  })
+}
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
